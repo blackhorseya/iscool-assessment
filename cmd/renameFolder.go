1161b597f8f4b078c1823647039a578c7391915b
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 12:07:41 +0800

    feat: introduce folder renaming functionality with cobra command
    - Add a new file `renameFolder.go` to the `cmd` directory
    - Define a `renameFolderCmd` cobra command to rename a folder
    - Add `fmt` and `github.com/spf13/cobra` imports
    - Add a `Run` function to print `renameFolder called`
    - Initialize the `renameFolderCmd` in the `init` function of `cmd/renameFolder.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ cmd/renameFolder.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// renameFolderCmd represents the renameFolder command
+var renameFolderCmd = &cobra.Command{
+	Use:   "rename-folder [username] [foldername] [new-folder-name]",
+	Short: "Rename a folder",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("renameFolder called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(renameFolderCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// renameFolderCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// renameFolderCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
