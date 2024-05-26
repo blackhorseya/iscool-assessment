69a11ba6c5e8db37451376f578add94fef5c2883
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 12:04:54 +0800

    feat: refactor folder creation functionality in CLI
    - Add a new file `cmd/createFolder.go` with a `createFolderCmd` command
    - Initialize `createFolderCmd` with a `Run` function printing `createFolder called`
    - Add `createFolderCmd` to `rootCmd`
    - Commented out sections for defining flags and configuration settings
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ cmd/createFolder.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// createFolderCmd represents the createFolder command
+var createFolderCmd = &cobra.Command{
+	Use:   "create-folder [username] [foldername] (description)",
+	Short: "create a new folder",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("createFolder called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createFolderCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createFolderCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createFolderCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
