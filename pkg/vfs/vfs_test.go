9a02443c5ea9a881c5f80503ef7da17f5e6bf398
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 14:02:15 +0800

    test: refactor folder creation functionality
    - Add tests for creating folders with different scenarios
    - Implement the CreateFolder function in the VirtualFileSystem
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/vfs/vfs_test.go
+++ pkg/vfs/vfs_test.go
@@ -282,3 +282,64 @@ func TestVirtualFileSystem_ListFiles(t *testing.T) {
 		})
 	}
 }
+
+func TestVirtualFileSystem_CreateFolder(t *testing.T) {
+	type fields struct {
+		Users map[string]*User
+	}
+	type args struct {
+		username    string
+		foldername  string
+		description string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "create folder successfully",
+			fields: fields{Users: map[string]*User{
+				"user1": {
+					Username: "user1",
+					Folders:  make(map[string]*Folder),
+				},
+			}},
+			args:    args{username: "user1", foldername: "folder1", description: "description1"},
+			wantErr: false,
+		},
+		{
+			name: "create folder with non-existing user",
+			fields: fields{Users: map[string]*User{
+				"user1": {
+					Username: "user1",
+					Folders:  make(map[string]*Folder),
+				},
+			}},
+			args:    args{username: "user2", foldername: "folder1", description: "description1"},
+			wantErr: true,
+		},
+		{
+			name: "create folder with existing folder name",
+			fields: fields{Users: map[string]*User{
+				"user1": {
+					Username: "user1",
+					Folders:  map[string]*Folder{"folder1": NewFolder("folder1", "description1")},
+				},
+			}},
+			args:    args{username: "user1", foldername: "folder1", description: "description1"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			vfs := &VFS{
+				Users: tt.fields.Users,
+			}
+			if err := vfs.CreateFolder(tt.args.username, tt.args.foldername, tt.args.description); (err != nil) != tt.wantErr {
+				t.Errorf("CreateFolder() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
