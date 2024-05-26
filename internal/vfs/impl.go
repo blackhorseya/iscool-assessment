48ddf2eeb041fc607d5174eeada3055c8d2a8214
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 16:11:03 +0800

    feat: implement Virtual File System methods and structs
    - Add `github.com/blackhorseya/iscool-assessment/entity/model` and `github.com/blackhorseya/iscool-assessment/pkg/vfs` imports
    - Define the `impl` struct for the Virtual File System
    - Implement methods for registering users, creating folders, deleting folders, listing folders, renaming folders, creating files, deleting files, and listing files with `panic` messages for each
    - All methods have a `TODO implement me` comment
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/vfs/impl.go
+++ internal/vfs/impl.go
@@ -1,1 +1,54 @@
 package vfs
+
+import (
+	"github.com/blackhorseya/iscool-assessment/entity/model"
+	"github.com/blackhorseya/iscool-assessment/pkg/vfs"
+)
+
+type impl struct {
+}
+
+// New is used to create a new VirtualFileSystem.
+func New() vfs.VirtualFileSystem {
+	return &impl{}
+}
+
+func (i *impl) RegisterUser(username string) (item *model.User, err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) CreateFolder(username, foldername, description string) (item *model.Folder, err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) DeleteFolder(username, foldername string) (err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) ListFolders(username string, sortBy string, order string) (items []*model.Folder, err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) RenameFolder(username, foldername, newFoldername string) (item *model.Folder, err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) CreateFile(username, foldername, filename, description string) (item *model.File, err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) DeleteFile(username, foldername, filename string) (err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) ListFiles(username, foldername string, sortBy string, order string) (items []*model.File, err error) {
+	// TODO implement me
+	panic("implement me")
+}
