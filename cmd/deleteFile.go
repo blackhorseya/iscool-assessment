cb0fd1807b11265108ab022d329abe44c9267681
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 13:28:20 +0800

    feat: improve file deletion functionality and error handling
    - Add validation for exact number of arguments in deleteFile command
    - Implement delete functionality for specified file in specified folder
    - Print error messages if deletion or saving fails
    - Print success message after successful deletion
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/deleteFile.go
+++ cmd/deleteFile.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -10,9 +11,26 @@ import (
 var deleteFileCmd = &cobra.Command{
 	Use:   "delete-file [username] [foldername] [filename]",
 	Short: "Delete a file from a folder",
+	Args:  cobra.ExactArgs(3),
 	Run: func(cmd *cobra.Command, args []string) {
-		// todo: 2024/5/26|sean|implement me
-		fmt.Println("deleteFile called")
+		username := args[0]
+		foldername := args[1]
+		filename := args[2]
+
+		err := vfs.DeleteFile(username, foldername, filename)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			return
+		}
+
+		err = vfs.SaveToFile(dataFile)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			return
+		}
+
+		// Delete [filename]in[username]/[foldername] successfully.
+		fmt.Printf("Delete %v in %v/%v successfully.\n", filename, username, foldername)
 	},
 }
 
