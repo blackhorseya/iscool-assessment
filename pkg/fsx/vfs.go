2016ec602228cf415b2e303949d2264b0497f8ac
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 11:47:39 +0800

    feat: implement user management functionality in VirtualFileSystem
    - Add UserManager interface implementation to VirtualFileSystem in vfs.go
    - Add RegisterUser, DeleteUser, and ListUsers methods to VirtualFileSystem in vfs.go
    - Add todo comments for future implementation in the new methods
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/fsx/vfs.go
+++ pkg/fsx/vfs.go
@@ -1,5 +1,7 @@
 package fsx
 
+var _ UserManager = &VirtualFileSystem{}
+
 // VirtualFileSystem represents the entire file system with user management
 type VirtualFileSystem struct {
 	Users map[string]*User `json:"users"`
@@ -10,4 +12,19 @@ func NewVFS() *VirtualFileSystem {
 	return &VirtualFileSystem{
 		Users: make(map[string]*User),
 	}
+}
+
+func (vfs *VirtualFileSystem) RegisterUser(username string) error {
+	// todo: 2024/5/26|sean|implement me
+	panic("implement me")
+}
+
+func (vfs *VirtualFileSystem) DeleteUser(username string) error {
+	// todo: 2024/5/26|sean|implement me
+	panic("implement me")
+}
+
+func (vfs *VirtualFileSystem) ListUsers() []string {
+	// todo: 2024/5/26|sean|implement me
+	panic("implement me")
 }
