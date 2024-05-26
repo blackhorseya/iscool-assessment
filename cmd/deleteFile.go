0bebd43035176dc35fd21d932c939e967e5def63
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 12:08:33 +0800

    feat: introduce file deletion command functionality
    - Add a new file `deleteFile.go` with a `deleteFileCmd` command to delete a file from a folder
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ cmd/deleteFile.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// deleteFileCmd represents the deleteFile command
+var deleteFileCmd = &cobra.Command{
+	Use:   "delete-file [username] [foldername] [filename]",
+	Short: "Delete a file from a folder",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("deleteFile called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteFileCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// deleteFileCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// deleteFileCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
