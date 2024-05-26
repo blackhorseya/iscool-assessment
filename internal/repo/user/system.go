ffc3b25ef202d6871e903d98375f0308fc87ecd4
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 17:42:37 +0800

    feat: implement user creation and folder creation in Register method
    - Add user creation and folder creation functionality to the Register method
    - Remove panic and implement user creation logic
    - Modify Register method to return the created user and handle errors
    - Add OS package import for folder creation functionality
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/repo/user/system.go
+++ internal/repo/user/system.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"os"
 
 	"github.com/blackhorseya/iscool-assessment/entity/model"
 	"github.com/blackhorseya/iscool-assessment/entity/repo"
@@ -19,8 +20,18 @@ func NewSystem(path string) (repo.UserManager, error) {
 }
 
 func (i *system) Register(ctx context.Context, username string) (item *model.User, err error) {
-	// TODO implement me
-	panic("implement me")
+	user, err := model.NewUser(username)
+	if err != nil {
+		return nil, err
+	}
+
+	// create a folder for the user
+	err = os.MkdirAll(i.path+"/"+username, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func (i *system) GetByUsername(ctx context.Context, username string) (item *model.User, err error) {
