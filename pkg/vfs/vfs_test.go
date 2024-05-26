842b6e690f4dcac3560cc655934c8127285b89c0
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 13:46:09 +0800

    test: improve file creation tests in virtual file system
    - Add tests for creating files in the virtual file system
    - Test creating a file successfully
    - Test creating a file with a non-existing user
    - Test creating a file with a non-existing folder
    - Test creating a file with an existing file name
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/vfs/vfs_test.go
+++ pkg/vfs/vfs_test.go
@@ -45,3 +45,84 @@ func TestVirtualFileSystem_SaveToFile(t *testing.T) {
 		})
 	}
 }
+
+func TestVirtualFileSystem_CreateFile(t *testing.T) {
+	type fields struct {
+		Users map[string]*User
+	}
+	type args struct {
+		username    string
+		foldername  string
+		filename    string
+		description string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "create file successfully",
+			fields: fields{Users: map[string]*User{
+				"user1": {
+					Username: "user1",
+					Folders:  map[string]*Folder{"folder1": NewFolder("folder1", "description1")},
+				},
+			}},
+			args:    args{username: "user1", foldername: "folder1", filename: "file1", description: "description1"},
+			wantErr: false,
+		},
+		{
+			name: "create file with non-existing user",
+			fields: fields{Users: map[string]*User{
+				"user1": {
+					Username: "user1",
+					Folders:  map[string]*Folder{"folder1": NewFolder("folder1", "description1")},
+				},
+			}},
+			args:    args{username: "user2", foldername: "folder1", filename: "file1", description: "description1"},
+			wantErr: true,
+		},
+		{
+			name: "create file with non-existing folder",
+			fields: fields{Users: map[string]*User{
+				"user1": {
+					Username: "user1",
+					Folders:  map[string]*Folder{"folder1": NewFolder("folder1", "description1")},
+				},
+			}},
+			args:    args{username: "user1", foldername: "folder2", filename: "file1", description: "description1"},
+			wantErr: true,
+		},
+		{
+			name: "create file with existing file name",
+			fields: fields{Users: map[string]*User{
+				"user1": {
+					Username: "user1",
+					Folders: map[string]*Folder{"folder1": {
+						Name:  "folder1",
+						Files: map[string]*File{"file1": NewFile("file1", "description1")},
+					}},
+				},
+			}},
+			args:    args{username: "user1", foldername: "folder1", filename: "file1", description: "description1"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			vfs := &VirtualFileSystem{
+				Users: tt.fields.Users,
+			}
+			if err := vfs.CreateFile(
+				tt.args.username,
+				tt.args.foldername,
+				tt.args.filename,
+				tt.args.description,
+			); (err != nil) != tt.wantErr {
+				t.Errorf("CreateFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
