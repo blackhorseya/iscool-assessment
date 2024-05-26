b9e65ca3aa746bc8a1a6eacda9162dbf6975bfeb
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 12:08:09 +0800

    feat: create a new command for file creation
    - Add a new file `createFile.go` in the `cmd` directory
    - Define a `createFileCmd` variable with a `cobra.Command` struct representing the createFile command
    - Implement the `Run` method for the `createFileCmd` command
    - Add the `createFileCmd` to the `rootCmd`
    - Initialize flags and configuration settings for the `createFileCmd`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ cmd/createFile.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// createFileCmd represents the createFile command
+var createFileCmd = &cobra.Command{
+	Use:   "create-file [username] [foldername] [filename] [description]?",
+	Short: "Create a file in a folder",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("createFile called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createFileCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createFileCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createFileCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
