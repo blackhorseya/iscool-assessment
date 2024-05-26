945686736d8f740fc7626752c7a2c1b1478d46c5
Sean Zheng <blackhorseya@gmail.com>
2024-05-26 16:13:25 +0800

    test: refactor VFS test setup and controllers
    - Add mock controllers for users and folders in vfs test
    - Update vfs test SetupTest and TearDownTest functions
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/vfs/impl_test.go
+++ internal/vfs/impl_test.go
@@ -3,14 +3,30 @@ package vfs
 import (
 	"testing"
 
+	"github.com/blackhorseya/iscool-assessment/entity/repo"
 	"github.com/blackhorseya/iscool-assessment/pkg/vfs"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/mock/gomock"
 )
 
 type suiteTester struct {
 	suite.Suite
 
-	vfs vfs.VirtualFileSystem
+	ctrl    *gomock.Controller
+	users   *repo.MockUserManager
+	folders *repo.MockFolderManager
+	vfs     vfs.VirtualFileSystem
+}
+
+func (s *suiteTester) SetupTest() {
+	s.ctrl = gomock.NewController(s.T())
+	s.users = repo.NewMockUserManager(s.ctrl)
+	s.folders = repo.NewMockFolderManager(s.ctrl)
+	s.vfs = New(s.users, s.folders)
+}
+
+func (s *suiteTester) TearDownTest() {
+	s.ctrl.Finish()
 }
 
 func TestAll(t *testing.T) {
